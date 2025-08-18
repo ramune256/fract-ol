c3235c126d8bcf8b5bdca67f9fbe4890803be9a7
ramune256 <shungo180620@outlook.jp>
2025-08-18 17:07:52 +0900

    fixed 3 test.sh

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 ./fractol julia 0.285 0.01
 ./fractol julia -0.74543 0.11301
 ./fractol julia -0.4 0.6
-./fractol julia -0.7 0.27015
+# ./fractol julia -0.7 0.27015
 ./fractol julia -0.8 0.156
 # ./fractol julia -0.123 0.745
 ./fractol julia 0.355 0.355
