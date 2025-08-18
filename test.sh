d96d71283d2e9dde1dc4e0e6c985d310f6362da9
ramune256 <shungo180620@outlook.jp>
2025-08-18 16:58:00 +0900

    fixed test.sh

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
 #!bin/bash
 ./fractol mandelbrot
-./fractol julia -0.7 0.27015
-./fractol julia -0.8 0.156
 ./fractol julia 0.285 0.01
 ./fractol julia -0.74543 0.11301
+./fractol julia -0.4 0.6
+./fractol julia -0.7 0.27015
+./fractol julia -0.8 0.156
 ./fractol julia -0.123 0.745
 ./fractol julia 0.355 0.355
 ./fractol julia -1.25 0.0
 ./fractol julia 0.45 0.1428
-./fractol julia -0.4 0.6
